ba8ab6e1a3b68f26493628d16acb657f8a97a572
nelsonabieno <=>
2019-08-06 22:36:04 +0100

    error handling and code refactor

--- app/controllers/geo_controller.rb
+++ app/controllers/geo_controller.rb
@@ -2,9 +2,14 @@ class GeoController < ApplicationController
   before_action :location_params, only: :inside
 
   def initialize
-    file = File.read('Given_areas.json')
-    @given_areas = JSON.parse(file)
     @inside = false
+
+    begin
+      file = File.read('Given_areas.json')
+      @given_areas = JSON.parse(file)
+    rescue StandardError => message
+      render json: {message: "Error reading file: #{message}"}, status: 500
+    end
   end
 
   def get_areas
@@ -12,31 +17,35 @@ class GeoController < ApplicationController
   end
 
   def inside
-    point_x = params[:points]["0"].to_f
-    point_y = params[:points]["1"].to_f
+    begin
+      point_x = params[:points]['0'].to_f
+      point_y = params[:points]['1'].to_f
 
-    @given_areas["features"].each do |feature_item|
-      coordinates_array = feature_item["geometry"]["coordinates"].first
-      x_coordinates_array = []
-      y_coordinates_array = []
+      @given_areas['features'].each do |feature_item|
+        coordinates_array = feature_item['geometry']['coordinates'].first
+        x_coordinates_array = []
+        y_coordinates_array = []
 
-      coordinates_array.each do |coordinate|
-        x_coordinates_array.push coordinate.first
-        y_coordinates_array.push coordinate.second
+        coordinates_array.each do |coordinate|
+          x_coordinates_array.push coordinate.first
+          y_coordinates_array.push coordinate.second
+        end
+
+        condition_x = point_x.between?(x_coordinates_array.min, x_coordinates_array.max)
+        condition_y = point_y.between?(y_coordinates_array.min, y_coordinates_array.max)
+
+        @inside = true if condition_x && condition_y # check if point x and y are located within the geometry
       end
 
-      if (point_x >= x_coordinates_array.min && point_x <= x_coordinates_array.max) &&
-          (point_y >= y_coordinates_array.min && point_y <= y_coordinates_array.max)
-        @inside = true
-      end
+      render json: { type_point: is_type_point?, inside: @inside  }, status: :ok
+
+    rescue StandardError => message
+      render json: { message: "Something went wrong: #{message}" }, status: 500
     end
-
-    render json: { type_point: is_type_point?, inside: @inside  }, status: :ok
   end
 
-
   def is_type_point?
-    params[:points].keys.count == 2 # check if point is an array of 2 elements x and y
+    params[:points].keys.count == 2 # check if inputted point is an array of 2 elements i.e. x and y
   end
 
   private
